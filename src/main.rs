7f53bd9a3cdd68471b8258e7004365a13c551783
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-03-17 12:37:08 +0200

    chore: minor refactorings to the current test code

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,16 @@ impl Svg {
 }
 
 fn main() {
-  let svg_str = "<svg version=\"1.2\"><path d=\"32423e1asfwe8192\"></path><path d=\"12e8fh92fh02dj3209\"/><rect width=\"10\"></rect></svg>";
-  let svg_el = Svg::from_str(svg_str).unwrap();
-  println!("Log: svg parsed successfully.");
+  let svg_str = "<svg version=\"1.2\"><path d=\"test\"></path><path d=\"test\"/><rect width=\"10\"></rect></svg>";
+  let svg_el = Svg::from_str(svg_str).expect("Failed to parse the SVG");
+  println!("Svg parsed successfully.");
 
   for node in &svg_el.root.children {
-    println!("Node: {}", node.as_element().unwrap().name);
+    let node_name = &node.as_element().unwrap().name;
+
+    println!("Node: {}", node_name);
   }
 
-  println!("Log: svg output:");
-  let svg_output = svg_el.to_string();
-  println!("{:?}", svg_output);
+  println!("Svg output:");
+  println!("{:?}", svg_el.to_string());
 }
-
