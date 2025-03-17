7b29c82a1739a1a9bb29478e6744b83f799b7355
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-03-17 16:44:54 +0200

    feat: implement the arbiter struct

---
+++ src/arbiter.rs
@@ -0,0 +1,69 @@
+use crate::svg::Svg;
+use std::error::Error;
+use crate::plugin::SingleElementPluginTrait;
+use crate::plugin::WholeSVGPluginTrait;
+use xmltree::Element;
+
+pub struct Arbiter {
+  single_element_plugins: Vec<Box<dyn SingleElementPluginTrait>>,
+  whole_svg_plugins: Vec<Box<dyn WholeSVGPluginTrait>>,
+}
+
+fn element_to_string(element: &Element) -> Result<String, Box<dyn Error>> {
+  let mut buffer = Vec::new();
+  element.write(&mut buffer)?;
+  let res = String::from_utf8(buffer)?;
+  Ok(res)
+}
+
+impl Arbiter {
+  pub fn new() -> Self {
+    Arbiter {
+      single_element_plugins: Vec::new(),
+      whole_svg_plugins: Vec::new(),
+    }
+  }
+
+  pub fn add_single_element_plugin(&mut self, plugin: Box<dyn SingleElementPluginTrait>) {
+    self.single_element_plugins.push(plugin);
+  }
+
+  pub fn add_whole_svg_plugin(&mut self, plugin: Box<dyn WholeSVGPluginTrait>) {
+    self.whole_svg_plugins.push(plugin);
+  }
+
+  pub fn process(&self, svg: &Svg) -> Result<Svg, Box<dyn Error>> {
+    let mut svg_clone = svg.clone();
+
+    for whole_svg_plugin in &self.whole_svg_plugins {
+      println!("Running whole SVG plugin");
+      let svg_output = whole_svg_plugin.process(&svg_clone)?;
+      if svg_output.to_string().unwrap().len() < svg_clone.to_string().unwrap().len() {
+        svg_clone = svg_output;
+        println!("Plugin reduced the size of the SVG");
+      }
+    }
+
+    for single_plugin in &self.single_element_plugins {
+      println!("Running single plugin");
+
+      let root_plugin_result = single_plugin.process(&svg_clone.root)?;
+
+      if element_to_string(&root_plugin_result).unwrap().len() < element_to_string(&svg_clone.root).unwrap().len() {
+        svg_clone.root = root_plugin_result;
+        println!("Plugin reduced the size of the element");
+      }
+
+      for node in &mut svg_clone.root.children {
+        let element = node.as_mut_element().unwrap();
+        let element_plugin_result = single_plugin.process(&element)?;
+        if element_to_string(&element_plugin_result).unwrap().len() < element_to_string(&element).unwrap().len() {
+          *element = element_plugin_result;
+          println!("Plugin reduced the size of the element");
+        }
+      }
+    }
+
+    Ok(svg_clone)
+  }
+}
