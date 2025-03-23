c213c683f80ca32f378b533ee411cfa124727620
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-03-23 20:05:46 +0200

    feat: implement css parser

--- src/css.rs
+++ src/css.rs
@@ -1,31 +1,60 @@
-use std::vec;
 use regex::Regex;
 
 #[derive(Debug)]
-struct CssProperty {
-  name: String,
-  value: String
+#[derive(Clone)]
+#[derive(PartialEq)]
+pub struct CSSProp {
+  pub name: String,
+  pub value: String
 }
 
 #[derive(Debug)]
-struct CssRule {
-  selector: String,
-  properties: Vec<CssProperty>
+#[derive(Clone)]
+#[derive(PartialEq)]
+pub struct CSSRule {
+  pub selector: String,
+  pub props: Vec<CSSProp>
 }
 
-pub struct CSSParser {}
+pub struct CSSParser;
 
 impl CSSParser {
-  pub fn from_string(input_str: &str) -> Vec<(String, String)> {
-    let re = Regex::new(r"(?s)([^{}]+)\s*\{([^}]*)\}").unwrap();
-    let mut results = Vec::new();
-
-    for cap in re.captures_iter(input_str) {
-      let selector = cap[1].trim().to_string();
-      let body = cap[2].trim().to_string();
-      results.push((selector, body));
+    pub fn from_string(input_str: &str) -> Result<Vec<CSSRule>, String> {
+        let rules = CSSParser::parse_rules(input_str)?;
+        Ok(rules)
     }
 
-    results
-  } 
+    fn parse_rules(input_str: &str) -> Result<Vec<CSSRule>, String> {
+        let re = Regex::new(r"(?s)([^{}]+)\s*\{([^}]*)\}").map_err(|e| e.to_string())?;
+        let mut results = Vec::new();
+
+        for cap in re.captures_iter(input_str) {
+            let selector = cap[1].trim().to_string();
+            let body = cap[2].trim().to_string();
+            let css_rule = CSSRule {
+                selector,
+                props: CSSParser::parse_props(body.as_str())?,
+            };
+            results.push(css_rule);
+        }
+
+        Ok(results)
+    }
+
+    fn parse_props(css_block: &str) -> Result<Vec<CSSProp>, String> {
+        let pattern = Regex::new(r"\s*([^:]+)\s*:\s*([^;]+)\s*;?").map_err(|e| e.to_string())?;
+        let mut props = Vec::new();
+
+        for cap in pattern.captures_iter(css_block) {
+            let prop_name = cap[1].trim().to_string();
+            let prop_value = cap[2].trim().to_string();
+            let prop = CSSProp {
+                name: prop_name,
+                value: prop_value,
+            };
+            props.push(prop);
+        }
+
+        Ok(props)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,43 +20,28 @@ use crate::plugins::CssToAttributesPlugin;
 use crate::css::CSSParser;
 
 fn main() {
-  let css = r#"
-        body { 
-            background-color: white; 
-            color: black; 
-        }
-        .container { 
-            width: 100%; 
-            height: auto; 
-        }
-    "#;
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let out_path = &args[2];
 
-    let parsed = CSSParser::from_string(&css);
-    for (selector, body) in parsed {
-        println!("Selector: {}\nBody: {}\n", selector, body);
-    }
-  /*let args: Vec<String> = env::args().collect();
-  let file_path = &args[1];
-  let out_path = &args[2];
+    let svg_string = fs::read_to_string(file_path).expect("Filed to read input SVG");
+    let svg_input = Svg::from_string(&svg_string).expect("Failed to parse the SVG");
 
-  let svg_string = fs::read_to_string(file_path).expect("Filed to read input SVG");
-  let svg_input = Svg::from_string(&svg_string).expect("Failed to parse the SVG");
+    println!("Input Size: {}", svg_input.to_string().unwrap().len());
 
-  println!("Input Size: {}", svg_input.to_string().unwrap().len());
-
-  let mut arbiter = Arbiter::new();
+    let mut arbiter = Arbiter::new();
   
-  // Run plugins
-  arbiter.add_single_element_plugin(Box::new(RemoveUnnecessaryAttrsPlugin {}));
-  arbiter.add_single_element_plugin(Box::new(ShapeToPathPlugin {}));
-  arbiter.add_single_element_plugin(Box::new(ApplyTransformsPlugin {}));
-  arbiter.add_whole_svg_plugin(Box::new(CssToAttributesPlugin {}));
+    // Run plugins
+    arbiter.add_single_element_plugin(Box::new(RemoveUnnecessaryAttrsPlugin {}));
+    arbiter.add_single_element_plugin(Box::new(ShapeToPathPlugin {}));
+    arbiter.add_single_element_plugin(Box::new(ApplyTransformsPlugin {}));
+    arbiter.add_whole_svg_plugin(Box::new(CssToAttributesPlugin {}));
 
-  let svg_output = arbiter.process(&svg_input).expect("Failed to process the SVG");
+    let svg_output = arbiter.process(&svg_input).expect("Failed to process the SVG");
 
-  println!("Output Size: {}", svg_output.to_string().unwrap().len());
+    println!("Output Size: {}", svg_output.to_string().unwrap().len());
 
-  let mut output_file = File::create(out_path).unwrap();
-  let _ = output_file.write_all(svg_output.to_string().unwrap().as_bytes());*/
+    let mut output_file = File::create(out_path).unwrap();
+    let _ = output_file.write_all(svg_output.to_string().unwrap().as_bytes());*/
 }
 
